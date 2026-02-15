e289840ceda2dd5bd8abfb4f7c369bfe9bf3b738
DamianR04 <damian.romeijn@hva.nl>
2026-02-16 00:00:18 +0100

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
 module.exports = function(eleventyConfig) {
-  // Kopieer het bestand en geef het een simpele naam in je public map
   eleventyConfig.addPassthroughCopy({
     "node_modules/js-confetti/dist/js-confetti.browser.js": "js/js-confetti.js"
   });
 
   eleventyConfig.addPassthroughCopy("src/styles");
   eleventyConfig.addPassthroughCopy("src/images");
+
+  return {    
+    dir: {
+      input: "src",
+      output: "public",
+      includes: "_includes"
+    }
+  };
 };
